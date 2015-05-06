9e6b4d194908872e8ccdc01feff1071155e6a6c9
@rgem52 <rgomez@hi-iberia.es>
2015-05-06 13:52:28 +0200

    postgres-base-storage

---
+++ postgres-base-storage.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Rober on 06/05/15.
+ */
+var pg = require('pg');
+var conString = "tcp://Rober@localhost:5432/Rober";
+var client = new pg.Client(conString);
+client.connect();
+
+client.query(
+    "INSERT INTO users " +
+    "(name, age) VALUES ($1, $2)",
+    ['Tom', 19]
+);
+
+var query = client.query(
+    "SELECT * FROM users WHERE age < $1",
+    [20],
+    function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        client.end();
+    }
+);
+query.on('row', function(row) {
+    console.log(row.name + ' .Age:' + row.age)
+});
+query.on('end', function() {
+    client.end();
+});
