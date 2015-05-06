57c5738ed7b2643c85542fb76a98ecb5cdd34367
@rgem52 <rgomez@hi-iberia.es>
2015-05-06 13:56:04 +0200

    postgres-base-storage-2

--- postgres-base-storage.js
+++ postgres-base-storage.js
@@ -2,7 +2,7 @@
  * Created by Rober on 06/05/15.
  */
 /**Postgres in localhost **/
-    
+
 var pg = require('pg');
 var conString = "tcp://Rober@localhost:5432/Rober";
 var client = new pg.Client(conString);
